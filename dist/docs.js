dbbede6b6c59e648a422353c610372a61d4ac5f4
TrejGun <trejgun@gmail.com>
2019-09-25 16:33:58 +0300

    add dist folder remove src from npm

--- dist/docs.js
+++ dist/docs.js
@@ -1,5 +1,4 @@
 "use strict";
-/* eslint-disable max-len */
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -12,12 +11,12 @@ const React = __importStar(require("react"));
 const core_1 = require("@material-ui/core");
 exports.Docs = props => {
     return (React.createElement(core_1.SvgIcon, Object.assign({}, props),
-        React.createElement("path", { d: "M20.4099375,6.57351563 L20.4099375,22.453875 C20.4099375,23.3078906 19.7172188,23.9999531 18.8638594,23.9999531 L4.5736875,23.9999531 C3.71967188,23.9999531 3.02760938,23.3078906 3.02760938,22.453875 L3.02760938,1.54607812 C3.02760938,0.6920625 3.719625,0 4.5736875,0 L13.8364219,0 L20.4099375,6.57351563 Z", id: "Path", fill: "#518EF8" }),
-        React.createElement("rect", { id: "Rectangle", fill: "#FFFFFF", x: "7.2740625", y: "12.0494062", width: "8.88871875", height: "1" }),
-        React.createElement("rect", { id: "Rectangle", fill: "#FFFFFF", x: "7.2740625", y: "14.25525", width: "8.88871875", height: "1" }),
-        React.createElement("rect", { id: "Rectangle", fill: "#FFFFFF", x: "7.2740625", y: "16.4611406", width: "8.88871875", height: "1" }),
-        React.createElement("rect", { id: "Rectangle", fill: "#FFFFFF", x: "7.2740625", y: "18.6670313", width: "6.32132812", height: "1" }),
-        React.createElement("polygon", { id: "Path", fill: "#3A5BBC", points: "14.7332812 6.4306875 20.4099375 8.76932813 20.4099375 6.57351562 17.1914531 5.62321875" }),
-        React.createElement("path", { d: "M20.4099375,6.57351563 L15.3825,6.57351563 C14.5284375,6.57351563 13.8364219,5.88145313 13.8364219,5.0274375 L13.8364219,0 L20.4099375,6.57351563 Z", id: "Path", fill: "#ACD1FC" })));
+        React.createElement("path", { fill: "#518EF8", d: "M20.4099375,6.57351563 L20.4099375,22.453875 C20.4099375,23.3078906 19.7172188,23.9999531 18.8638594,23.9999531 L4.5736875,23.9999531 C3.71967188,23.9999531 3.02760938,23.3078906 3.02760938,22.453875 L3.02760938,1.54607812 C3.02760938,0.6920625 3.719625,0 4.5736875,0 L13.8364219,0 L20.4099375,6.57351563 Z" }),
+        React.createElement("rect", { fill: "#FFFFFF", x: "7.2740625", y: "12.0494062", width: "8.88871875", height: "1" }),
+        React.createElement("rect", { fill: "#FFFFFF", x: "7.2740625", y: "14.25525", width: "8.88871875", height: "1" }),
+        React.createElement("rect", { fill: "#FFFFFF", x: "7.2740625", y: "16.4611406", width: "8.88871875", height: "1" }),
+        React.createElement("rect", { fill: "#FFFFFF", x: "7.2740625", y: "18.6670313", width: "6.32132812", height: "1" }),
+        React.createElement("polygon", { fill: "#3A5BBC", points: "14.7332812 6.4306875 20.4099375 8.76932813 20.4099375 6.57351562 17.1914531 5.62321875" }),
+        React.createElement("path", { fill: "#ACD1FC", d: "M20.4099375,6.57351563 L15.3825,6.57351563 C14.5284375,6.57351563 13.8364219,5.88145313 13.8364219,5.0274375 L13.8364219,0 L20.4099375,6.57351563 Z" })));
 };
 //# sourceMappingURL=docs.js.map
--- dist/photos.js
+++ dist/photos.js
@@ -1,5 +1,4 @@
 "use strict";
-/* eslint-disable max-len */
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -12,13 +11,13 @@ const React = __importStar(require("react"));
 const core_1 = require("@material-ui/core");
 exports.Photos = props => {
     return (React.createElement(core_1.SvgIcon, Object.assign({}, props),
-        React.createElement("polygon", { id: "Path", fill: "#D32E2A", points: "18.0857344 6 18.0857344 12 16.2857344 13.644 14.4 12 12.7714219 11.3142656 14.3714062 7.75828125" }),
-        React.createElement("polygon", { id: "Path", fill: "#3A5BBC", points: "18 18.0571406 12 18.0571406 11.9714063 17.9714531 10.5999844 16.7011406 11.9714063 14.4394219 12.6857344 12.742875 16.6857187 14.1582656" }),
-        React.createElement("polygon", { id: "Path", fill: "#FBBB00", points: "12.0285938 6.02859375 13.5142969 7.72973438 12.0285938 9.56057812 11.3142656 11.257125 7.5714375 9.38685937 6 5.94285938 12 5.94285938" }),
-        React.createElement("polygon", { id: "Path", fill: "#28B446", points: "11.2285781 12.6857344 9.58570313 16.2582656 5.91426562 18 5.91426562 12 7.68571875 10.9011562 9.6 12" }),
-        React.createElement("polygon", { id: "Path", fill: "#518EF8", points: "24 12 18 18.0571406 12.6857344 12.742875 14.4 12 18.0857344 12" }),
-        React.createElement("polygon", { id: "Path", fill: "#91C646", points: "11.9714063 17.9714531 11.9714063 24 5.91426563 18 11.2285781 12.6857344 11.9714063 14.4 11.9714063 14.4394219" }),
-        React.createElement("polygon", { id: "Path", fill: "#FFD837", points: "11.3142656 11.257125 9.6 12 5.91426563 12 0 12 6 5.94285938" }),
-        React.createElement("polygon", { id: "Path", fill: "#F14336", points: "18.0857344 6 12.7714219 11.3142656 12.0285938 9.6 12.0285938 9.56057813 12.0285938 6.02859375 12.0285938 0" })));
+        React.createElement("polygon", { fill: "#D32E2A", points: "18.0857344 6 18.0857344 12 16.2857344 13.644 14.4 12 12.7714219 11.3142656 14.3714062 7.75828125" }),
+        React.createElement("polygon", { fill: "#3A5BBC", points: "18 18.0571406 12 18.0571406 11.9714063 17.9714531 10.5999844 16.7011406 11.9714063 14.4394219 12.6857344 12.742875 16.6857187 14.1582656" }),
+        React.createElement("polygon", { fill: "#FBBB00", points: "12.0285938 6.02859375 13.5142969 7.72973438 12.0285938 9.56057812 11.3142656 11.257125 7.5714375 9.38685937 6 5.94285938 12 5.94285938" }),
+        React.createElement("polygon", { fill: "#28B446", points: "11.2285781 12.6857344 9.58570313 16.2582656 5.91426562 18 5.91426562 12 7.68571875 10.9011562 9.6 12" }),
+        React.createElement("polygon", { fill: "#518EF8", points: "24 12 18 18.0571406 12.6857344 12.742875 14.4 12 18.0857344 12" }),
+        React.createElement("polygon", { fill: "#91C646", points: "11.9714063 17.9714531 11.9714063 24 5.91426563 18 11.2285781 12.6857344 11.9714063 14.4 11.9714063 14.4394219" }),
+        React.createElement("polygon", { fill: "#FFD837", points: "11.3142656 11.257125 9.6 12 5.91426563 12 0 12 6 5.94285938" }),
+        React.createElement("polygon", { fill: "#F14336", points: "18.0857344 6 12.7714219 11.3142656 12.0285938 9.6 12.0285938 9.56057813 12.0285938 6.02859375 12.0285938 0" })));
 };
 //# sourceMappingURL=photos.js.map
